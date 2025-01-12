dcdf28105ca5f5650d9f129adf2608c3db6afab7
Jens Krumsieck <j.krumsieck@outlook.de>
2025-01-13 00:05:48 +0100

    added logging

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -7,10 +7,26 @@ use axum::{
     Router,
 };
 use sqlx::sqlite::SqlitePoolOptions;
+use tracing::info;
 use std::{fs, net::SocketAddr};
 use tokio::net::TcpListener;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub async fn serve() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!(
+                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    env!("CARGO_PKG_NAME")
+                )
+                .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()?;
+
     let pool = get_database().await?;
 
     let app = Router::new()
@@ -18,11 +34,13 @@ pub async fn serve() -> anyhow::Result<()> {
         .route("/articles", get(models::get_articles))
         .route("/articles", post(models::create_article))
         .route("/articles/{slug}", get(models::get_article_by_slug))
+        .layer(TraceLayer::new_for_http())
         .with_state(pool);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await?;
 
+    info!("Starting server on {}", listener.local_addr()?);
     axum::serve(listener, app).await?;
     Ok(())
 }
@@ -47,6 +65,7 @@ async fn get_database() -> anyhow::Result<sqlx::SqlitePool> {
         .max_connections(5)
         .connect(&db_url)
         .await?;
+    info!("Established database connection");
     Ok(pool)
 }
 
