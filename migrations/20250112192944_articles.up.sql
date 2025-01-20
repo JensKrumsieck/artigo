c43d58000cce200615fd40028c0dd122216860ed
Jens Krumsieck <j.krumsieck@outlook.de>
2025-01-20 20:08:30 +0000

    change to pqsql

--- migrations/20250112192944_articles.up.sql
+++ migrations/20250112192944_articles.up.sql
@@ -1,14 +1,14 @@
 -- Add up migration script here
 CREATE TABLE
     articles (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id SERIAL PRIMARY KEY,
         slug VARCHAR(255) UNIQUE NOT NULL,
         title VARCHAR(255) NOT NULL,
         body TEXT NOT NULL,
         hero_image VARCHAR(255),
-        tags JSON DEFAULT '[]',
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+        tags TEXT[] DEFAULT '{}',
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
     );
 
 -- create dummy
@@ -20,5 +20,5 @@ VALUES
         'Hello World',
         'This is my first article',
         'https://example.com/hello-world.jpg',
-        '["hello", "world"]'
+        ARRAY['hello', 'world']
     );
--- src/models.rs
+++ src/models.rs
@@ -1,18 +1,22 @@
-use super::AppError;
-use axum::{extract::{Path, State}, http::StatusCode, Json};
+use crate::AppError;
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    Json,
+};
 use chrono::DateTime;
 use serde::{Deserialize, Serialize};
-use sqlx::{FromRow, SqlitePool};
+use sqlx::{FromRow, PgPool};
 
 #[derive(FromRow, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Article {
-    pub id: i64,
+    pub id: i32,
     pub slug: String,
     pub title: String,
     pub body: String,
     pub hero_image: Option<String>,
-    pub tags: Option<String>, // make vec in pg
+    pub tags: Option<Vec<String>>,
     #[serde(with = "chrono::serde::ts_seconds")]
     pub created_at: DateTime<chrono::Utc>,
     #[serde(with = "chrono::serde::ts_seconds")]
@@ -26,11 +30,11 @@ pub struct ArticleRequest {
     pub title: String,
     pub body: String,
     pub hero_image: Option<String>,
-    pub tags: Option<String>,
+    pub tags: Option<Vec<String>>,
 }
 
 pub async fn get_articles(
-    State(pool): State<SqlitePool>,
+    State(pool): State<PgPool>,
 ) -> Result<(StatusCode, Json<Vec<Article>>), AppError> {
     let articles = sqlx::query_as("SELECT * FROM articles")
         .fetch_all(&pool)
@@ -39,10 +43,10 @@ pub async fn get_articles(
 }
 
 pub async fn get_article_by_slug(
-    State(pool): State<SqlitePool>,
+    State(pool): State<PgPool>,
     Path(slug): Path<String>,
 ) -> Result<(StatusCode, Json<Article>), AppError> {
-    let article = sqlx::query_as("SELECT * FROM articles WHERE slug = ?")
+    let article = sqlx::query_as("SELECT * FROM articles WHERE slug = $1")
         .bind(slug)
         .fetch_one(&pool)
         .await?;
@@ -50,10 +54,10 @@ pub async fn get_article_by_slug(
 }
 
 pub async fn create_article(
-    State(pool): State<SqlitePool>,
+    State(pool): State<PgPool>,
     Json(article): Json<ArticleRequest>,
 ) -> Result<(StatusCode, Json<Article>), AppError> {
-    let article = sqlx::query_as("INSERT INTO articles (slug, title, body, hero_image, tags) VALUES (?, ?, ?, ?, ?) RETURNING *")
+    let article = sqlx::query_as("INSERT INTO articles (slug, title, body, hero_image, tags) VALUES ($1, $2, $3, $4, $5) RETURNING *")
         .bind(article.slug)
         .bind(article.title)
         .bind(article.body)
@@ -65,11 +69,11 @@ pub async fn create_article(
 }
 
 pub async fn update_article(
-    State(pool): State<SqlitePool>,
+    State(pool): State<PgPool>,
     Path(slug): Path<String>,
     Json(article): Json<ArticleRequest>,
 ) -> Result<(StatusCode, Json<Article>), AppError> {
-    let article = sqlx::query_as("UPDATE articles SET slug = ?, title = ?, body = ?, hero_image = ?, tags = ? WHERE slug = ? RETURNING *")
+    let article = sqlx::query_as("UPDATE articles SET slug = $1, title = $2, body = $3, hero_image = $4, tags = $5 WHERE slug = $6 RETURNING *")
         .bind(article.slug)
         .bind(article.title)
         .bind(article.body)
@@ -82,10 +86,10 @@ pub async fn update_article(
 }
 
 pub async fn delete_article(
-    State(pool): State<SqlitePool>,
+    State(pool): State<PgPool>,
     Path(slug): Path<String>,
 ) -> Result<StatusCode, AppError> {
-    sqlx::query("DELETE FROM articles WHERE slug = ?")
+    sqlx::query("DELETE FROM articles WHERE slug = $1")
         .bind(slug)
         .execute(&pool)
         .await?;
